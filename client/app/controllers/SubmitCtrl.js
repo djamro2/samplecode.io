f6e798c0be26afa70ce1a32a85f44e66be9ab89a
Dan Jamrozik <djamrozik@ymail.com>
2015-08-02 19:42:05 -0500

    added submit samplecode form

---
+++ client/app/controllers/SubmitCtrl.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var controllers = controllers || angular.module('SampleCode.controllers', []);
+
+controllers.controller('SubmitController', ['$scope', function($scope){
+
+	var self = this;
+	
+	self.init = function(){
+		//nothing yet	
+	};
+	
+	self.init();
+	
+}]);
